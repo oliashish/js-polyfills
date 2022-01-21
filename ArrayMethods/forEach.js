d7eb8576bada1a62e3e2d5719a2422f9605dff58
oliashish <oliashish3@gmail.com>
2022-01-22 00:26:31 +0530

    feat: created polyfill for Array.forEach

---
+++ ArrayMethods/forEach.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 4, 5, 3, 2, 3, 5, 6, 10]; // can be array of any data type or mix of datatypes
+
+Array.prototype.customForEach = function (callback) {
+  // this will point to arr that we created
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i);
+  }
+};
+
+arr.customForEach((currentValue, index) => {
+  console.log(currentValue);
+});
